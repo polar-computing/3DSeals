a72e0bfe90a2c52746d2535cb8010ac898bb0e33
BentoCG <bentocollares@gmail.com>
2016-07-19 15:19:11 -0400

    Add files via upload

--- Watershed segmentation.py
+++ Watershed segmentation.py
@@ -30,34 +30,80 @@ def perm_func (arg1):
         dist = math.hypot(arg1[i+1][0,0] - arg1[i][0,0], arg1[i+1][0,1] - arg1[i][0,1] )
         perm = perm + dist
     return perm
+    
+def gamma_search(image):
+    """
+    Gamma correction function (from http://stackoverflow.com/questions/11211260/gamma-correction-power-law-transformation)
+    """
+    def gamma(image,gamma = 0.5):
+        img_float = np.float32(image)
+        max_pixel = np.max(img_float)
+        #image pixel normalisation
+        img_normalised = img_float/max_pixel
+        #gamma correction exponent calulated
+        gamma_corr = np.log(img_normalised)*gamma
+        #gamma correction being applied
+        gamma_corrected = np.exp(gamma_corr)*255.0
+        #conversion to unsigned int 8 bit
+        gamma_corrected = np.uint8(gamma_corrected)
+        return gamma_corrected
+
+    trash = image[:].copy()
+
+    # Generate a range of gamma levels to test against
+    gamma_values = np.arange(0.1, 5.1, 0.1)
+    output_images = []
+    edge_images = []
+
+    # Create directory to save images
+    folder_name = "output"
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+    for gamma_level, index in zip(gamma_values, range(gamma_values.size)):
+        gamma_data = gamma(input_data, gamma_level)                                                                       #gamma
+        histogram_data = cv2.equalizeHist(src=gamma_data, dst=trash)                                                      #histogram
+        # TODO clean up noise
+        #blur_data = cv2.bilateralFilter(histogram_data, 15, 75, 75)
+        threshold_data = cv2.adaptiveThreshold(histogram_data, 240, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 201, 2) #threshold
+        output_images.append(threshold_data)
+        file_name = "image_{0:03d}.jpg".format(index)
+        # run edge detection
+        edge_data = cv2.Canny(threshold_data, 0, 255)
+        # get edge pixels
+        edge_images.append(edge_data)
+        #cv2.imwrite(os.path.join(folder_name, file_name), threshold_data) #save in output folder
+
+    max_pixels = 0
+    max_gamma = None
+    max_index = 0
+
+    for edge_data, index in zip(edge_images, range(len(edge_images))):
+        pixel_sum = np.sum(edge_data)
+        if pixel_sum > max_pixels :
+            max_pixels = pixel_sum
+            max_gamma = edge_data
+            max_index = index
+    print max_index
+    return output_images[max_index]
 
 def watershed_seg(img, plot=True, file_name="segmented_seal.png"):
     """
     Segments an image and returns the contours of every patch along with their
     complexity scores. Saves a segmented seal image on the workspace  
     """
-    # create a copy of our image to extract rectangles
-    img_copy = image[:].copy()
+    img_copy = img[:].copy()
+    #gamma correction
+    img = gamma_search(img)
     
-    # convert to grayscale
-    gray = rgb2gray(image)
-    
-    # adaptive thresholding for distances
-    adpt_thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,
-                  cv2.THRESH_BINARY,11,2)
-    
-    # binary thresholding 
-    bin_thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)[1]
-    
-    # distance transform
-    D = ndimage.distance_transform_edt(adpt_thresh)
+    D = ndimage.distance_transform_edt(img)
     localMax = peak_local_max(D, indices=False, min_distance=30,
-    labels=adpt_thresh)
-    
+    labels=img)
+
     # perform a connected component analysis on the local peaks,
     # using 8-connectivity, then appy the Watershed algorithm
     markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
-    labels = watershed(-D, markers, mask=bin_thresh)
+    labels = watershed(-D, markers, mask=img)
     
     print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
     
@@ -93,7 +139,7 @@ def watershed_seg(img, plot=True, file_name="segmented_seal.png"):
             tag = tag + 1
             x1,y1,w,h = cv2.boundingRect(c)
             # remove white splotches
-            if img_copy[y1:(y1+h), x1:(x1+w)].mean() < 170:
+            if img_copy[y1:(y1+h), x1:(x1+w)].mean() < 150:
                 splotches_rect.append([tag, img_copy[y1:(y1+h), x1:(x1+w)]])
                 splotches_cont.append(c)
                 complexity.append(perm_func(c))
@@ -118,4 +164,5 @@ def watershed_seg(img, plot=True, file_name="segmented_seal.png"):
     # returns contours and their complexity scores
     return(splotches_cont, complexity)
 
-watershed_seg(image)
+out = watershed_seg(output)
+

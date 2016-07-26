2f9fde4a44276ca18c5859861f511530d39b1269
aborowicz <alex.borowicz@stonybrook.edu>
2016-07-26 18:07:58 -0400

    Added background extraction
    added back_extract.py

--- Watershed segmentation.py
+++ Watershed segmentation.py
@@ -33,7 +33,6 @@ from skimage.measure import perimeter
 
 # Watershed segmentation ---- WORKS BEST ON OPENCV 3.1.0
 
-
 def perim_func (arg1):
     """
     Calculates 'patch complexity', based on the ratio of patch perimeter to patch
@@ -57,9 +56,52 @@ def gamma(img, gamma = 0.5):
     #conversion to unsigned int 8 bit
     gamma_corrected = np.uint8(gamma_corrected)
     return gamma_corrected   
-def gamma_search(img, invert=False, save_img=False):
+
+def back_extract (img):
+    '''
+    Attempts to find the background and turn it black. Equalizes the histogram,
+    boosts gamma way up to 15 such that the only contour is the seal (usually),
+    blurs, then finds that contour, builds a filled polygon from the point, 
+    and then multiplies the (inverted) boolean values by the original image 
+    such that the background (black, 0) turns all corresponding background 
+    pixels in the original black as well.
+    
+    Requires: cv2, numpy as np
+    '''
+    trash = img[:].copy()    
+    eq_img = cv2.equalizeHist(src=img, dst=trash)
+    gammed = gamma(eq_img, gamma=15)
+    blur = gammed
+    cv2.GaussianBlur(src=gammed, dst=blur, ksize=(35,35), sigmaX=0, sigmaY=0 )         
+    cont = cv2.findContours(blur, cv2.RETR_EXTERNAL,
+           cv2.CHAIN_APPROX_SIMPLE)[-2]
+    areaArray = []
+    for i, c in enumerate(cont):
+        area = cv2.contourArea(c)
+        areaArray.append(area)
+    sorteddata = sorted(zip(areaArray, cont), key = lambda x: x[0], 
+                        reverse=True)
+    largest1 = sorteddata[0][1]
+    points1 = np.array([point[0] for point in largest1])
+    points2 = [0,0]
+    if len(sorteddata) > 1 : #Some images don't have 2 segments 
+        largest2 = sorteddata[1][1]
+        points2 = np.array([point[0] for point in largest2])
+    else: largest2 = np.asarray((0,0))
+    blank = np.zeros(shape = img.shape)
+    if len(points2) > 2 : #If there're two segments
+        filled = cv2.fillPoly(blank, [points1, points2], 1)
+    else:
+        filled = cv2.fillPoly(blank, [points1], 1)
+    boole = -np.bool8(filled) #inverts so background is 0
+    boole = np.uint8(boole)
+    masked = img*boole
+    return masked    
+    
+def gamma_search(img, invert=False, save_img=False): 
     """
-    Gamma correction function (from http://stackoverflow.com/questions/11211260/gamma-correction-power-law-transformation)
+    Gamma correction function (from http://stackoverflow.com/questions/11211260
+    /gamma-correction-power-law-transformation)
     """
     trash = img[:].copy()
 
@@ -74,7 +116,7 @@ def gamma_search(img, invert=False, save_img=False):
         os.mkdir(folder_name)
 
     for gamma_level, index in zip(gamma_values, range(gamma_values.size)):
-        gamma_data = gamma(image, gamma_level)                                                                       #gamma
+        gamma_data = gamma(img, gamma_level)                                                                       #gamma
         histogram_data = cv2.equalizeHist(src=gamma_data, dst=trash)                                                      #histogram
         if invert:
             threshold_data = cv2.adaptiveThreshold(histogram_data, 240, 
@@ -104,22 +146,26 @@ def gamma_search(img, invert=False, save_img=False):
             max_gamma = edge_data
             max_index = index
     return output_images[max_index]
-
-def watershed_seg(img, plot_comp=True, show_img=True, file_name="segmented_seal.png"):
+    
+def watershed_seg(img, plot_comp=True, show_img=True, inv=False, file_name="segmented_seal.png"):
     """
     Segments an image and returns the contours of every patch along with their
     complexity scores and their labels to locate specific patches on the image.
     The segmented seal image is saved on the workspace with the name specified
     on 'file_name' argument. 
     """
+    img = back_extract(img)
+        
     # check input!
     img_copy = img[:].copy()
     # gamma correction
     img_copy2 = img[:].copy()
-    img = gamma_search(img)
-    
+    if inv:    
+        img = gamma_search(img, invert=True)
+    else:
+        img = gamma_search(img, invert=False)
         
-    cv2.imshow("gamma corrected image", neg)  
+    cv2.imshow("gamma corrected image", img)  
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
@@ -155,15 +201,20 @@ def watershed_seg(img, plot_comp=True, show_img=True, file_name="segmented_seal.
         mask[labels == label] = 255
     
         # detect contours in the mask and grab the largest one
+        #cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        #    cv2.CHAIN_APPROX_SIMPLE)[-2]
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)[-2]
+               cv2.CHAIN_APPROX_SIMPLE)[-2]
+        #print cnts[0]
+        
         c = max(cnts, key=cv2.contourArea)
        
         # draw a circle enclosing the object
         # exclude patches that are too large or too small
         # size threshold is based on the contour area
         area = cv2.contourArea(c)
-        if area > (img.size / 10000) and area < (img.size / 100):
+        ############ TEMP BELOW
+        if area > (img.size / 10000) and area < (img.size / 50):
             x1,y1,w,h = cv2.boundingRect(c)
             # remove white splotches
             if img_copy[y1:(y1+h), x1:(x1+w)].mean() < 170:
@@ -173,8 +224,9 @@ def watershed_seg(img, plot_comp=True, show_img=True, file_name="segmented_seal.
                 complexity.append(perim_func(c))
                 if show_img:
                     cv2.drawContours(img_copy2, [c], -1, (255, 0, 0), 2)
-                    cv2.putText(img_copy2, "#{}".format(tag), (int(x1) - 10, int(y1)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)    
+                    cv2.putText(img_copy2, "#{}".format(tag), (int(x1) - 10, 
+                                int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 
+                                0.6, (0, 0, 255), 2)    
     print("[INFO] {} segments retained:".format(tag)) 
             
     if show_img:
@@ -188,12 +240,13 @@ def watershed_seg(img, plot_comp=True, show_img=True, file_name="segmented_seal.
         import seaborn as sns
     
         sns.set(rc={"figure.figsize": (18, 9)}, font_scale=3 )
-        comp_dist = sns.distplot(complexity, bins=15, axlabel="patch complexity")
+        comp_dist = sns.distplot(complexity, bins=15, 
+                                 axlabel="patch complexity")
     # write it into a file
     cv2.imwrite(file_name, img_copy2)
     # returns contours and their complexity scores
     return(splotches_cont, complexity, tags)
 
 # imread does not crash with invalid input
-image = cv2.imread("test1.jpg", 0)
-out1 = watershed_seg(image, file_name="eroded_seal.png")
+image = cv2.imread("images/test4.jpg", 0)
+out1 = watershed_seg(image, show_img=True, file_name="eroded_seal.png")

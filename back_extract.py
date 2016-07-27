71ffe8877cecfddb2646c829562ed0ec167a04d0
aborowicz <alex.borowicz@stonybrook.edu>
2016-07-27 15:31:20 -0400

    Incorporated grabCut
    Added grabCut. It now runs the blunt axe background extractor, modifies the image, runs grabCut, and modifies the modified image, resulting in a much better and more accurate foreground extraction.

--- back_extract.py
+++ back_extract.py
@@ -11,6 +11,10 @@ Created on Mon Jul 21 2016
     the background (black, 0) turns all corresponding background pixels in the 
     original black as well.
     
+    Then runs grabCut, a foreground extractor which is generally better but
+    misses small pieces (that would certainly be segmented in findContour) 
+    which are smoothed out by the blunt axe of the rest of back_extract
+    
     Requires: cv2, numpy as np, gamma_adjust (local tool on git)
 """
 import cv2
@@ -19,8 +23,9 @@ from gamma_adjust import gamma
 
 def back_extract (img):
 
-    trash = img[:].copy()    
-    eq_img = cv2.equalizeHist(src=img, dst=trash)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    trash = gray_img[:].copy()    
+    eq_img = cv2.equalizeHist(src=gray_img, dst=trash)
     gammed = gamma(eq_img, gamma=15)
     blur = gammed
     cv2.GaussianBlur(src=gammed, dst=blur, ksize=(35,35), sigmaX=0, sigmaY=0 )         
@@ -39,12 +44,45 @@ def back_extract (img):
         largest2 = sorteddata[1][1]
         points2 = np.array([point[0] for point in largest2])
     else: largest2 = np.asarray((0,0))
-    blank = np.zeros(shape = img.shape)
+    blank = np.zeros(shape = gray_img.shape)
     if len(points2) > 2 : #If there're two segments
         filled = cv2.fillPoly(blank, [points1, points2], 1)
     else:
         filled = cv2.fillPoly(blank, [points1], 1)
-    boole = -np.bool8(filled) #inverts so background is 0
+    boole = ~np.bool8(filled) #inverts so background is 0
     boole = np.uint8(boole)
-    masked = img*boole
+    masked = gray_img*boole
+        ######## Secondary: GrabCut
+
+    mask = np.zeros(img.shape[:2],np.uint8)
+    
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    
+    rect = (0,0,img.shape[1]-1, len(img)-1)
+    
+    cv2.grabCut(img,mask,rect,bgdModel,fgdModel,2,cv2.GC_INIT_WITH_RECT)
+    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+    masked2 = img*mask2[:,:,np.newaxis]
+    masked2 = cv2.cvtColor(masked2, cv2.COLOR_BGR2GRAY)
+    masked = masked2*boole
+    
+    cv2.imshow("masked img", masked)  
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
+    return masked 
+    
+    ######## Secondary: GrabCut
+
+    mask = np.zeros(img.shape[:2],np.uint8)
+    
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    
+    rect = (0,0,img.shape[1]-1, len(img)-1)
+    
+    cv2.grabCut(img,mask,rect,bgdModel,fgdModel,2,cv2.GC_INIT_WITH_RECT)
+    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+    masked = img*mask2[:,:,np.newaxis]*masked
+    
     return masked
